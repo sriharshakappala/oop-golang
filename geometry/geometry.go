40c36605f48fdf3457345020d6fdf79dd1728622
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2018-06-18 22:27:49 +0530

    init example and logging using log.fatal

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"oop/geometry/rectangle"
 )
 
+var rectLen, rectWid float64 = -1, 7
+
+func init() {
+	fmt.Println("Main package initialized")
+	if rectLen < 0 {
+		log.Fatal("length is less than 0")
+	}
+	if rectWid < 0 {
+		log.Fatal("width is less than 0")
+	}
+}
+
 func main() {
-	var rectLen, rectWid float64 = 6, 7
 	fmt.Printf("Area of rectangle %.2f", rectangle.Area(rectLen, rectWid))
 	fmt.Println()
 	fmt.Printf("Diagonal of rectangle %.2f", rectangle.Diagonal(rectLen, rectWid))
